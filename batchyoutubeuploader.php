56fe206b7ff797b83b54c8fb59e5ad6c049cad1d
mAAdhaTTah <jamesorodig@gmail.com>
2014-04-17 18:24:58 -0400

    Modifying the output to create a .csv that matches the input
    We should probably handle this better (well, the whole thing kinda sucks right now), but this will do for now

--- batchyoutubeuploader.php
+++ batchyoutubeuploader.php
@@ -12,7 +12,7 @@ $OAUTH2_CLIENT_ID = getenv('oauth2_client_id');
 $OAUTH2_CLIENT_SECRET = getenv('oauth2_client_secret');
 $OAUTH2_REDIRECT_URI = getenv('oauth2_redirect_uri');
 $videoDir = getenv('videodir');
-$logFile = 'output.log';
+$logFile = 'completed.csv';
 
 // Set up the Google Client
 $SCOPES = array('https://www.googleapis.com/auth/youtube',
@@ -75,75 +75,82 @@ $csv_array = csv_to_array('videos.csv');
  */
 foreach($csv_array as $videoInfo) {
 	$videoPath = $videoDir . '/' . $videoInfo['filename'];
-	if(empty($videoInfo['youtube_url']) && file_exists($videoPath)) {
-		try {
-			$startTime = time();
-			// Create a snippet with title, description, tags and category ID
-			// Create an asset resource and set its snippet metadata and type.
-			print "Uploading {$videoInfo['title']}\n";
-			$snippet = new Google_Service_YouTube_VideoSnippet();
-			$snippet->setTitle($videoInfo['title']);
-			// $snippet->setDescription($videoInfo['description']);
-			$snippet->setCategoryId("28"); // @todo make this selectable in some way; 28 = Science & Technology
-
-			// Set the video's status to "unlisted".
-			$status = new Google_Service_YouTube_VideoStatus();
-			$status->privacyStatus = "unlisted";
-
-			// Associate the snippet and status objects with a new video resource.
-			$video = new Google_Service_YouTube_Video();
-			$video->setSnippet($snippet);
-			$video->setStatus($status);
-
-			// Specify the size of each chunk of data, in bytes. Set a higher value for
-			// reliable connection as fewer chunks lead to faster uploads. Set a lower
-			// value for better recovery on less reliable connections.
-			$chunkSizeBytes = 1 * 1024 * 1024;
-
-			// Setting the defer flag to true tells the client to return a request which can be called
-			// with ->execute(); instead of making the API call immediately.
-			$client->setDefer(true);
-
-			// Create a request for the API's videos.insert method to create and upload the video.
-			$insertRequest = $youtube->videos->insert("status,snippet", $video);
-
-			// Create a MediaFileUpload object for resumable uploads.
-			$media = new Google_Http_MediaFileUpload(
-				$client,
-				$insertRequest,
-				'video/*',
-				null,
-				true,
-				$chunkSizeBytes
-				);
-			$filesize = filesize($videoPath);
-			$media->setFileSize($filesize);
-
-			// Read the media file and upload it chunk by chunk.
-			$status = false;
-			$handle = fopen($videoPath, "rb");
-			while (!$status && !feof($handle)) {
-				$chunk = fread($handle, $chunkSizeBytes);
-				$status = $media->nextChunk($chunk); // @todo deal with exception
-				$currentProgress = $media->getProgress();
-				$progressPercentage = ($currentProgress/$filesize) * 100;
-				print($progressPercentage . "%\n");
-			}
-
-			fclose($handle);
-
-			// If you want to make other calls after the file upload, set setDefer back to false
-			$client->setDefer(false);
-			print $videoInfo['title'] . " uploaded\n";
-			print "https://www.youtube.com/watch?v=" . $status->id . "\n";
-			$logData = $videoInfo['filename'] . ',' . $startTime . ',' . $status->status->uploadStatus . ',' . "https://www.youtube.com/watch?v={$status->id}" . ',' . time() . "\n";
-		} catch(Google_Exception $e) {
-			$exceptionMsg = "Google service Exception: " . $e->getCode() . "; message: "	.$e->getMessage();
-			print($exceptionMsg);
-			$logData = $videoInfo['filename'] . ',' . $startTime . ',' . $exceptionMsg . ',' . 'NA' . ',' . time() . "\n";
-			exit(print_r($media));
-		}
+	if(!file_exists($videoPath)) {
+		$logData = $videoInfo['entry_id'] . ',' . '"' . $videoInfo['title'] . '"' . ',' . $videoInfo['filename'] . ',' . "File does not exist\n";
 		file_put_contents($logFile, $logData, FILE_APPEND | LOCK_EX);
+		continue;
+	}
+	if(!empty($videoInfo['youtube_url'])) {
+		$logData = $videoInfo['entry_id'] . ',' . '"' . $videoInfo['title'] . '"' . ',' . $videoInfo['filename'] . ',' . $videoInfo['youtube_url'] . "\n";
+		file_put_contents($logFile, $logData, FILE_APPEND | LOCK_EX);
+		continue;
+	}
+	try {
+		$startTime = time();
+		// Create a snippet with title, description, tags and category ID
+		// Create an asset resource and set its snippet metadata and type.
+		print "Uploading {$videoInfo['title']}\n";
+		$snippet = new Google_Service_YouTube_VideoSnippet();
+		$snippet->setTitle($videoInfo['title']);
+		// $snippet->setDescription($videoInfo['description']);
+		$snippet->setCategoryId("28"); // @todo make this selectable in some way; 28 = Science & Technology
+
+		// Set the video's status to "unlisted".
+		$status = new Google_Service_YouTube_VideoStatus();
+		$status->privacyStatus = "unlisted";
+
+		// Associate the snippet and status objects with a new video resource.
+		$video = new Google_Service_YouTube_Video();
+		$video->setSnippet($snippet);
+		$video->setStatus($status);
+
+		// Specify the size of each chunk of data, in bytes. Set a higher value for
+		// reliable connection as fewer chunks lead to faster uploads. Set a lower
+		// value for better recovery on less reliable connections.
+		$chunkSizeBytes = 1 * 1024 * 1024;
+
+		// Setting the defer flag to true tells the client to return a request which can be called
+		// with ->execute(); instead of making the API call immediately.
+		$client->setDefer(true);
+
+		// Create a request for the API's videos.insert method to create and upload the video.
+		$insertRequest = $youtube->videos->insert("status,snippet", $video);
+
+		// Create a MediaFileUpload object for resumable uploads.
+		$media = new Google_Http_MediaFileUpload(
+			$client,
+			$insertRequest,
+			'video/*',
+			null,
+			true,
+			$chunkSizeBytes
+			);
+		$filesize = filesize($videoPath);
+		$media->setFileSize($filesize);
+
+		// Read the media file and upload it chunk by chunk.
+		$status = false;
+		$handle = fopen($videoPath, "rb");
+		while (!$status && !feof($handle)) {
+			$chunk = fread($handle, $chunkSizeBytes);
+			$status = $media->nextChunk($chunk); // @todo deal with exception
+			$currentProgress = $media->getProgress();
+			$progressPercentage = ($currentProgress/$filesize) * 100;
+			print($progressPercentage . "%\n");
+		}
+
+		fclose($handle);
+
+		// If you want to make other calls after the file upload, set setDefer back to false
+		$client->setDefer(false);
+		print $videoInfo['title'] . " uploaded\n";
+		print "https://www.youtube.com/watch?v=" . $status->id . "\n";
+		$logData = $videoInfo['entry_id'] . ',' . '"' . $videoInfo['title'] . '"' . ',' . $videoInfo['filename'] . ',' . "https://www.youtube.com/watch?v={$status->id}\n";
+		file_put_contents($logFile, $logData, FILE_APPEND | LOCK_EX);
+	} catch(Google_Exception $e) {
+		$exceptionMsg = "Google service Exception: " . $e->getCode() . "; message: "	.$e->getMessage();
+		print($exceptionMsg);
+		exit('Likely need to refresh the token!');
 	}
 }
 
